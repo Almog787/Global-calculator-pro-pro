704d90fe029bf74cb89a74e39b03e669ef78d6bd
Almog787 <143160546+Almog787@users.noreply.github.com>
2026-08-08 19:01:37 +0000

    chore: wipe all files from branch

--- fix_syntax2.cjs
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-let code = fs.readFileSync('src/contexts/i18n.tsx', 'utf8');
-
-// I will just replace all instances of unescaped quotes within the fr strings.
-// A regex to replace any single quote preceded by a letter and followed by a letter, which isn't already escaped.
-code = code.replace(/([a-zA-Z])'([a-zA-Z])/g, "$1\\'$2");
-
-fs.writeFileSync('src/contexts/i18n.tsx', code);
